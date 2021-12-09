6eb62e7427eb81013a7bc40a26ce334cfc2c4369
Mahadev <Mahadev.clever@gmail.com>
2021-12-09 20:11:03 +0530

    project setup

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
-const express = require("express");
-require("dotenv/config");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
 app.use(express.json());
-app.use(morgan("tiny"));
-const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: Number,
-});
-const Product = mongoose.model("Product", productSchema);
+app.use(morgan('tiny'));
+
+
+//Routes
+const categoriesRoutes = require('./routes/categories.js');
+const productsRoutes = require('./routes/products.js');
+const usersRoutes = require('./routes/users.js');
+const ordersRoutes = require('./routes/orders.js');
+
 const api = process.env.API_URL;
 
-mongoose
-  .connect("mongodb://localhost/eshop-database")
-  .then(() => {
-    console.log("Database Connection is ready");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-app.listen(3000, () => {
-  console.log("server is running http://localhost:3000");
-});
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
+
+// Database
+mongoose.connect("mongodb://localhost/eshop-database")
+.then(()=>{
+  console.log('Database Connection is ready...')
+})
+.catch((err)=> {
+  console.log(err);
+})
+const PORT = 5000;
+// server
+app.listen(PORT,()=>console.log(`server running on http://localhost:${PORT}/api/v1/products/`));
